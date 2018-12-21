9dcc87c5519b0a7ba9030ff952b682dc3dd086be
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2018-12-21 14:04:55 -0800

    Beginning to add support for more items

--- src/App.js
+++ src/App.js
@@ -4,48 +4,62 @@ import React from 'react';
 import Wormhole from './Wormhole';
 import Content from './components/Content';
 import Message from './components/Message';
-import {PIP_ID, VIDEO_URL} from './Constants';
+import {PIP_ID, VIDEOS_1, VIDEOS_2} from './Constants';
 import styles from './App.module.css';
 
-function getId(src, context) {
-  return `${context}-${src}`;
+type AppState = {|
+  page: number,
+  pages: Array<[typeof VIDEOS_1, typeof VIDEOS_2]>,
+|};
+
+class App extends React.Component<void, AppState> {
+  state = {
+    page: 0,
+    pages: [VIDEOS_1, VIDEOS_2],
+  };
+
+  render() {
+    const {page, pages} = this.state;
+    return (
+      <Wormhole>
+        <Content>
+          {pages[page].map(({src, width, height}) => (
+            <Wormhole.Item
+              key={src}
+              id={src}
+              renderPlaceholder={(_, __, renderItemToTarget) => (
+                <div
+                  onClick={() => {
+                    renderItemToTarget(src, src);
+                  }}
+                  className={styles.placeholder}
+                  style={{width: 200, height: 355}}>
+                  Return video
+                </div>
+              )}>
+              {(target, targets, renderItemToTarget) => {
+                return (
+                  <Message
+                    width={200}
+                    height={355}
+                    src={src}
+                    onClick={() => {
+                      if (target === PIP_ID) {
+                        renderItemToTarget(src, src);
+                      } else {
+                        renderItemToTarget(src, PIP_ID);
+                      }
+                    }}
+                  />
+                );
+              }}
+            </Wormhole.Item>
+          ))}
+        </Content>
+        <Wormhole.Target id={PIP_ID} className={styles.pip} />
+      </Wormhole>
+    );
+  }
 }
 
-export default () => (
-  <Wormhole>
-    <Content>
-      <Wormhole.Item
-        id={getId(VIDEO_URL, 'content')}
-        renderPlaceholder={(_, __, renderItemToTarget) => (
-          <div
-            onClick={() => {
-              const id = getId(VIDEO_URL, 'content');
-              renderItemToTarget(id, id);
-            }}
-            className={styles.placeholder}
-            style={{width: 200, height: 355}}>
-            Return video
-          </div>
-        )}>
-        {(target, targets, renderItemToTarget) => {
-          return (
-            <Message
-              width={200}
-              height={355}
-              src={VIDEO_URL}
-              onClick={() => {
-                const id = getId(VIDEO_URL, 'content');
-                if (target === PIP_ID) {
-                  renderItemToTarget(id, id);
-                } else {
-                  renderItemToTarget(id, PIP_ID);
-                }
-              }}
-            />
-          );
-        }}
-      </Wormhole.Item>
-    </Content>
-    <Wormhole.Target id={PIP_ID} className={styles.pip} />
-  </Wormhole>
-);
+export default App;
--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,37 @@
 export const PIP_ID = 'pip';
 
-export const VIDEO_URL = 'https://cdn.discordapp.com/attachments/267040578556919808/522213885948985380/video.mov';
+export const VIDEOS_1 = [
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/522213885948985380/video.mov',
+    width: 720,
+    height: 1280,
+  },
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/513092159713312789/IMG_7965.mov',
+    width: 720,
+    height: 1280,
+  },
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/513091924370915353/IMG_7971.mov',
+    width: 720,
+    height: 1280,
+  },
+];
+
+export const VIDEOS_2 = [
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/513065590928441370/video.mov',
+    width: 720,
+    height: 1280,
+  },
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/509134684270231565/video.mov',
+    width: 720,
+    height: 1280,
+  },
+  {
+    src: 'https://cdn.discordapp.com/attachments/267040578556919808/500057152300384261/video.mov',
+    width: 720,
+    height: 1280,
+  },
+];
