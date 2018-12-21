53280f1cdc33aa02ced455e48fd9130ec477a2a0
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2018-12-21 01:28:56 -0800

    Add playsInline

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export default ({src, width, height}) => <video width={width} height={height} src={src} autoPlay muted loop />;
+export default ({src, width, height}) => (
+  <video width={width} height={height} src={src} autoPlay muted loop playsInline />
+);
